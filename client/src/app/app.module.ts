138477e392d7873238f3c5d962bbb54543c0af56
gohurali <gohurali@live.com>
2021-04-21 13:13:52 -0700

    ++ admin,employee,user,login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AdmidComponent } from './admid/admid.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    EmployeeComponent,
+    AdmidComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
