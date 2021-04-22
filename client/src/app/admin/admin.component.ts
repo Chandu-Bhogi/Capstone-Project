95e227086c1a8fc7335dcc0bccd0075b26cbb74a
Christian Seyoum <Chriscoding96@gmail.com>
2021-04-22 17:32:53 -0400

    admin can add and delete employee. not yet connected to database

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  employee = new FormGroup ({
+    fName: new FormControl(),
+    lName: new FormControl(),
+    email: new FormControl()
+  })
+
+  employeeID = new FormGroup({
+    id:new FormControl()
+  })
+
+  count = 1
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addEmployee() {
+    console.log(this.employee.value)
+    alert(this.makeEmployeeID(this.employee.value.fName, this.employee.value.lName, this.count) + "\n" +
+    this.makePassword(this.employee.value.email))
+  }
+
+  deleteEmployee() {
+    console.log(this.employeeID.value)
+  }
+
+  makePassword(email:String):String {
+    let min = Math.ceil(100)
+    let max = Math.floor(1000)
+    let num = Math.floor(Math.random() * (max - min) + min)
+
+    return email.split("@")[0] + "" + num
+  }
+
+  makeEmployeeID(fName:String, lName:String, count:Number):String {
+    return fName.charAt(0).toLowerCase() + lName.toLowerCase() + count
+  }
 }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {UserSignupComponent} from './user-signup/user-signup.component'
 import {LoginComponent} from './login/login.component'
 import {UserComponent} from './user/user.component'
 import { EditProfileComponent } from './user/edit-profile/edit-profile.component';
+import {AdminComponent} from './admin/admin.component'
 
 const routes: Routes = [
   {path:"", component:LoginComponent},
   {path:"signup", component:UserSignupComponent},
   {path:"user", component:UserComponent},
-  {path:"editProfile",component:EditProfileComponent}
+  {path:"editProfile",component:EditProfileComponent},
+  {path:"admin",component:AdminComponent}
 ];
 
 @NgModule({
