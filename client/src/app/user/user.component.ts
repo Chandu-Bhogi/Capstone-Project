faab92a51e0771e5a89f107462f3a2eec75ecb30
Christian Seyoum <Chriscoding96@gmail.com>
2021-04-22 22:12:33 -0400

    user panel added not connected to db yet

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -7,13 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  items:String[] = ['item1', 'item2', 'item3', 'item4']
+  cart:String[] = []
+  checkCart = false
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  logout_user(){
-    // send a logout request
+  logout_user() {
+    alert("LogOut")
   }
 
+  addToCart(item:String) {
+    this.cart.push(item)
+  }
+
+  showCart() {
+    this.checkCart ? this.checkCart = false : this.checkCart = true
+  }
+
+  removeFromCart(item:String) {
+    let index = this.cart.indexOf(item)
+    this.cart.splice(index, 1)
+  }
 }
