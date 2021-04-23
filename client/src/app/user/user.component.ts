9802fe3e30bcbb0f9a772ef013f8e87c8428cebe
Christian Seyoum <Chriscoding96@gmail.com>
2021-04-22 23:47:58 -0400

    user panel edited

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -9,7 +9,8 @@ export class UserComponent implements OnInit {
 
   items:String[] = ['item1', 'item2', 'item3', 'item4']
   cart:String[] = []
-  checkCart = false
+  showCart = false
+  showEdit = false
 
   constructor() { }
 
@@ -24,8 +25,19 @@ export class UserComponent implements OnInit {
     this.cart.push(item)
   }
 
-  showCart() {
-    this.checkCart ? this.checkCart = false : this.checkCart = true
+  showCartBtn() {
+    this.showCart ? this.showCart = false : this.showCart = true
+    this.showEdit = false
+  }
+
+  showEditBtn() {
+    this.showEdit ? this.showEdit = false : this.showEdit = true
+    this.showCart = false
+  }
+
+  homeBtn() {
+    this.showCart = false
+    this.showEdit = false
   }
 
   removeFromCart(item:String) {
