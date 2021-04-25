c8172fa1a8f2b4a87cf6fc2d0ce1898b903307bb
gohurali <gohurali@live.com>
2021-04-24 18:55:35 -0700

    Backup commit Using TDF instead of MDF, mdf is in comments if needed, deleting in next commit

--- client/src/app/employee/edit-employee-profile/edit-employee-profile.component.ts
+++ client/src/app/employee/edit-employee-profile/edit-employee-profile.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEmployeeProfileComponent implements OnInit {
 
+  password1Res?:string
+  password2Res?:string
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submitNewPassword(updatePasswordRef:any){
+    console.log(updatePasswordRef)
+
+    // Check if current passoword matches
+
+    // check if the new passwords match
+    if(updatePasswordRef['f_newPassword1'] != updatePasswordRef['f_newPassword2']){
+      console.log("ok")
+      this.password1Res = "❌"
+      this.password2Res = "❌"
+    }
   }
 
 }
--- client/src/app/employee/send-request/send-request.component.ts
+++ client/src/app/employee/send-request/send-request.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-send-request',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-request.component.css']
 })
 export class SendRequestComponent implements OnInit {
+  
+  show:boolean = false
+  temp_products:any = {
+    "products":[
+      {pname:"apple",quantity:54},
+      {pname:"orange",quantity:8},
+      {pname:"grapes",quantity:19},
+      {pname:"cherries",quantity:213},
+      {pname:"peaches",quantity:182},
+      {pname:"cocacola",quantity:562},
+      {pname:"sprite",quantity:923},
+      {pname:"mountaindew",quantity:231}
+    
+    ]
+  }
+  products:Array<any> = []
+
+  productQuantityForm?:any
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.productQuantityForm = new FormGroup({
+      product: new FormGroup({
+        name:new FormControl(),
+        quantity:new FormControl()
+      })
+    })
   }
 
+  sendRequest(){
+    console.log(this.productQuantityForm.value)
+  }
+
+  sendRequest2(productRef:any){
+    console.log("TDF")
+    console.log(productRef)
+
+  }
 }
